2c2410cf6e057fb9c2791c59b82fc689ad295361
Vu Van Dung <dung@tensorplex.ai>
2025-04-23 14:26:08 +0800

    check whether a category is declared in CI
    Signed-off-by: Vu Van Dung <dung@tensorplex.ai>

--- generate_subnets.py
+++ generate_subnets.py
@@ -1,6 +1,10 @@
 import os
 import json
 
+# Load categories for checking
+categories_file = "./categories.json"
+categories = [category["name"] for category in json.load(open(categories_file))["categories"]]
+
 # Define the base directory containing the subnet data folders
 base_directory = "./data"
 
@@ -16,6 +20,14 @@ for subdir in os.listdir(base_directory):
                 filepath = os.path.join(subdir_path, filename)
                 with open(filepath, "r") as file:
                     subnet_data = json.load(file)
+
+                    # Check all categories in subnet_data["categories"] is in the categories list
+                    if "categories" in subnet_data:
+                        for category in subnet_data["categories"]:
+                            if category not in categories:
+                                print(f"Category '{category}' in subnet {subdir} not found in {categories_file}")
+                                exit(1)
+
                     # Add subnet_id based on the subdirectory name
                     subnet_data["subnet_id"] = int(subdir)
                     subnets.append(subnet_data)
